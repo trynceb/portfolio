ab461e5b36af7e4ec0ca71cfe3d63b65c6c22b2b
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-12 15:11:12 -0400

    make Navbar responsive

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import Navbar from "./scenes/Navbar"
-import { useState } from "react";
-// import useMediaQuery from "./hooks/useMediaQuery";
+import DotGroup from "./scenes/DoGgroup"
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState('home')
-  // const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) setIsTopOfPage(true)
+      if(window.scrollY !== 0) setIsTopOfPage(false)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
 
   return (
     <div className="app bg-deep-blue">
       <Navbar
+        isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup 
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+      </div>
     </div>
   );
 }
--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,4 +1,4 @@
-// import { useState } from "react"
+import { useState } from "react"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 import useMediaQuery from "../hooks/useMediaQuery"
 
@@ -17,12 +17,13 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
     )
 }
 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
-    // const [isMenuToggled, setIsMenuToggled] = useState(false)
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
+    const [isMenuToggled, setIsMenuToggled] = useState(false)
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)")
+    const navbarBackground = isTopOfPage ? "" : "bg-red"
 
     return (
-        <nav className={`z-40 w-full fixed top-0 py-6`} >
+        <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`} >
             <div className="flex items-center justify-between mx-auto w-5/6">
                 <h4 className="font-playfair text-3xl font-bold">TB</h4>
 
@@ -34,8 +35,65 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
                             selectedPage={selectedPage}
                             setSelectedPage={setSelectedPage}
                         />
+                        <Link 
+                            page="Skills"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link 
+                            page="Projects"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link 
+                            page="Contact"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
                     </div>
-                ) : (<div></div>)}
+                ) : (
+                    <button
+                        className="rounded-full bg-red p-2"
+                        onClick={() => setIsMenuToggled(!isMenuToggled)}
+                    >
+                        <img alt="menu-icon" scr="../assets/menu-icon.svg" />
+                    </button>
+                )}
+
+                {/* MOBILE MENU POPUP */}
+                {!isAboveSmallScreens && isMenuToggled && (
+                    <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px">
+                        {/* CLOSE ICON */}
+                        <div className="flex justify-end p-12">
+                            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                                <img alt="close-icon" scr="../assets/close-icon.svg" />
+                            </button>
+                        </div>
+                        {/* MENU ITEMS */}
+                        <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+                            <Link 
+                                page="Home"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link 
+                                page="Skills"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link 
+                                page="Projects"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link 
+                                page="Contact"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                        </div>
+                    </div>
+                )}
             </div>
         </nav>
     )
