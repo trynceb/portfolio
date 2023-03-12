d1ca83fe4bfb6e4f6f7c7fc4868cb89176711818
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-12 14:31:32 -0400

    create Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import Navbar from "./scenes/Navbar"
+import { useState } from "react";
+// import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home')
+  // const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <div className="app bg-deep-blue">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
